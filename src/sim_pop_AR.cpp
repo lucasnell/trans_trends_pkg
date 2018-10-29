76379bd73ecbdaa41776af687e0a4b38abc55de7
Lucas Nell <lucas@lucasnell.com>
2018-10-29 15:45:54 -0500

    Cleaned fxns, using pcg instead of sitmo

--- src/sim_pop_AR.cpp
+++ src/sim_pop_AR.cpp
@@ -1,5 +1,5 @@
 #include <RcppArmadillo.h>
-#include <sitmo.h>  // sitmo::prng_engine
+#include <pcg/pcg_random.hpp> // pcg prng
 #include <random>  // normal distribution
 #include <vector>  // vector class
 #ifdef _OPENMP
@@ -7,48 +7,37 @@
 #endif
 
 #include "repts_types.h"
+#include "pcg.h"  // pcg seeding
 
 using namespace Rcpp;
 
 
-/*
- One population simulated using AR1 process.
- N in this case is already logged
- */
-//[[Rcpp::export]]
-arma::vec sim_pop_ar(const arma::vec& X, const double& N0,
-                     const double& b0, const double& b1, const double& rho,
-                     const double& sigma, const uint& seed) {
-    uint n_gen = X.n_elem - 1;
-    arma::vec N(n_gen + 1);
-    N(0) = N0;
-    
-    std::normal_distribution<double> rnorm_distr(0.0, sigma);
-    sitmo::prng_engine engine(seed);
-    
-    for (uint t = 0; t < n_gen; t++) {
-        N(t+1) = b0 + b1 * X(t+1) + rho * ( N(t) - b0 - b1 * X(t) );
-        N(t+1) += rnorm_distr(engine);
-    }
-    
-    return N;
-}
+// /*
+//  One population simulated using AR1 process.
+//  N in this case is already logged
+//  */
+// //[[Rcpp::export]]
+// arma::vec sim_pop_ar(const arma::vec& X, const double& N0,
+//                      const double& b0, const double& b1, const double& rho,
+//                      const double& sigma) {
+//     uint32 n_gen = X.n_elem - 1;
+//     arma::vec N(n_gen + 1);
+//     N(0) = N0;
+//
+//     std::normal_distribution<double> rnorm_distr(0.0, sigma);
+//     pcg32 engine = seeded_pcg();
+//
+//     for (uint32 t = 0; t < n_gen; t++) {
+//         N(t+1) = b0 + b1 * X(t+1) + rho * ( N(t) - b0 - b1 * X(t) );
+//         N(t+1) += rnorm_distr(engine);
+//     }
+//
+//     return N;
+// }
 
 
 
 
-
-/*
- Get one N value based on input parameters.
- Does NOT include process or observation error.
- */
-inline double N_t1(const double& b0, const double& b1,
-                   const double& rho,
-                   const double& Xt, const double& Xt1,
-                   const double& Nt) {
-    double Nt1 = b0 + b1 * Xt1 + rho * ( Nt - b0 - b1 * Xt );
-    return Nt1;
-}
 
 
 /*
@@ -58,8 +47,8 @@ inline double N_t1(const double& b0, const double& b1,
 void check_dims(const arma::mat& X, const arma::mat& N0_mat,
                 const arma::mat& b0_mat, const arma::mat& b1_mat,
                 const arma::mat& rho_mat, const arma::cube& vcv_cube) {
-    
-    uint n_locs = X.n_cols;
+
+    uint32 n_locs = X.n_cols;
     if (N0_mat.n_cols != n_locs) {
         stop("N0_mat should have the same number of columns as does X");
     }
@@ -75,8 +64,8 @@ void check_dims(const arma::mat& X, const arma::mat& N0_mat,
     if (vcv_cube.n_slices != n_locs) {
         stop("vcv_cube should have the same number of slices as X has columns");
     }
-    
-    uint n_spp = N0_mat.n_rows;
+
+    uint32 n_spp = N0_mat.n_rows;
     if (N0_mat.n_rows != n_spp) {
         stop("N0_mat should have the same number of rows as does N0");
     }
@@ -92,7 +81,7 @@ void check_dims(const arma::mat& X, const arma::mat& N0_mat,
     if (vcv_cube.n_rows != n_spp || vcv_cube.n_cols != n_spp) {
         stop("vcv_cube should have the same number of rows and columns as N0 has rows");
     }
-    
+
     return;
 }
 
@@ -100,18 +89,18 @@ void check_dims(const arma::mat& X, const arma::mat& N0_mat,
 /*
  Turn variance-covariance matrix into matrix that can be used to generate random
  normal deviates with same covariance structure
- 
+
  "a vector of independent normal random variables,
   when multiplied by the transpose of the Cholesky deposition of [vcv] will
   have covariance matrix equal to [vcv]."
  */
 arma::cube make_chol_decomp(const arma::cube& vcv_cube) {
-    
+
     arma::cube chol_decomp(vcv_cube);
-    for (uint i = 0; i < vcv_cube.n_slices; i++) {
+    for (uint32 i = 0; i < vcv_cube.n_slices; i++) {
         chol_decomp.slice(i) = arma::chol(vcv_cube.slice(i)).t();
     }
-    
+
     return chol_decomp;
 }
 
@@ -119,129 +108,147 @@ arma::cube make_chol_decomp(const arma::cube& vcv_cube) {
 
 
 //' Multiple populations simulated using AR1 process.
-//' 
+//'
 //' Input and output N values are logged.
-//' The `X` matrix should have rows associated with a given time point and
-//' columns associated with a given location.
-//' The `vcv_cube` cube should have rows and columns associated with a given species,
-//' and slices associated with a given location.
-//' All other input matrices should have rows associated with a given species and
-//' columns associated with a given location.
-//' `obs_sigma` contains the standard deviations of observation error for each species.
-//' The output array will have rows associated with a given time point,
-//' columns associated with a given species, and 
+//' All input matrices other than `X` and `vcv_cube` should have rows associated
+//' with a given species and columns associated with a given location.
+//' See descriptions for `X` and `vcv_cube`.
+//'
+//' @param X Matrix of environmental variable.
+//'     It should have rows associated with a given time point and
+//'     columns associated with a given location.
+//' @param N0_mat Matrix of starting population abundances (`log(# individuals)`)
+//'     by species and location.
+//' @param b0_mat Matrix of $\beta_0$ (the population-abundance intercept) values
+//'     by species and location.
+//' @param b1_mat Matrix of $\beta_1$ (the effect of $X$ on $N$) values
+//'     by species and location.
+//' @param rho_mat Matrix of growth rates by species and location.
+//' @param vcv_cube Cube representing variance-covariance matrices for process error
+//'     among species, one matrix for each location.
+//'     It should have rows and columns associated with a given species,
+//'     and slices associated with a given location.
+//' @param obs_sigma Vector of standard deviations of observation error for each species.
+//' @param n_cores Number of cores to use. Defaults to 1.
+//'
+//'
+//'
+//' @return A 3-dimensional array.
+//' The output will have rows associated with a given time point,
+//' columns associated with a given species, and
 //' slices associated with a given location.
-//' 
-//' 
-//' 
-//' 
+//'
+//'
+//'
+//'
 //' @export
-//' 
-//' 
+//'
+//'
 //[[Rcpp::export]]
 arma::cube sim_pops_ar(const arma::mat& X, const arma::mat& N0_mat,
                        const arma::mat& b0_mat, const arma::mat& b1_mat,
                        const arma::mat& rho_mat, const arma::cube& vcv_cube,
-                       const arma::vec& obs_sigma, const uint& n_cores = 1) {
-    
+                       const arma::vec& obs_sigma, const uint32& n_cores = 1) {
+
     check_dims(X, N0_mat, b0_mat, b1_mat, rho_mat, vcv_cube);
-    
+
     // For random number generator
-    const std::vector<uint> seeds(as<std::vector<uint>>(runif(n_cores, 0, 2147483647)));
+    const std::vector<std::vector<uint64>> seeds = mc_seeds(n_cores);
     // For turning ~N(0,1) to multivariate with given covariance matrix
     const arma::cube chol_decomp(make_chol_decomp(vcv_cube));
 
-    const uint n_time(X.n_rows);
-    const uint n_locs(X.n_cols);
-    const uint n_spp(N0_mat.n_rows);
-    
+    const uint32 n_time(X.n_rows);
+    const uint32 n_locs(X.n_cols);
+    const uint32 n_spp(N0_mat.n_rows);
+
     arma::cube N(n_time, n_spp, n_locs);
     N.tube(arma::span(0), arma::span()) = N0_mat;
-    
+
     #ifdef _OPENMP
     #pragma omp parallel default(shared) num_threads(n_cores) if(n_cores > 1)
     {
     #endif
-    
-    uint active_seed;
-    
+
+    std::vector<uint64> active_seeds;
+
     // Write the active seed per core or just write one of the seeds.
     #ifdef _OPENMP
-    uint active_thread = omp_get_thread_num();
-    active_seed = seeds[active_thread];
+    uint32 active_thread = omp_get_thread_num();
+    active_seeds = seeds[active_thread];
     #else
-    active_seed = seeds[0];
+    active_seeds = seeds[0];
     #endif
-    
-    sitmo::prng_engine engine(active_seed);
+
+    pcg32 engine = seeded_pcg(active_seeds);
     // Random normal distribution:
     std::normal_distribution<double> rnorm_distr(0.0, 1.0);
-    
+
     // Parallelize the Loop
     #ifdef _OPENMP
     #pragma omp for schedule(static)
     #endif
-    for (uint loc = 0; loc < n_locs; loc++) {
+    for (uint32 loc = 0; loc < n_locs; loc++) {
         const arma::mat& cd_loc(chol_decomp.slice(loc));
         const arma::vec& b0s(b0_mat.col(loc));
         const arma::vec& b1s(b1_mat.col(loc));
         const arma::vec& rhos(rho_mat.col(loc));
         const arma::vec& Xs(X.col(loc));
         arma::mat& Ns(N.slice(loc));
-        for (uint t = 0; t < n_time - 1; t++) {
-            arma::vec rnd(n_spp);
+        for (uint32 t = 0; t < n_time - 1; t++) {
+            arma::vec proc_rnd(n_spp);
             arma::vec obs_rnd(n_spp);
-            for (uint sp = 0; sp < n_spp; sp++) {
-                Ns(t+1,sp) = N_t1(b0s(sp), b1s(sp), rhos(sp), Xs(t), Xs(t+1), Ns(t,sp));
-                rnd(sp) = rnorm_distr(engine);
+            for (uint32 sp = 0; sp < n_spp; sp++) {
+                Ns(t+1,sp) = b0s(sp) + b1s(sp) * Xs(t+1) + rhos(sp) * (
+                    Ns(t,sp) - b0s(sp) - b1s(sp) * Xs(t));
+                proc_rnd(sp) = rnorm_distr(engine);
                 obs_rnd(sp) = rnorm_distr(engine) * obs_sigma(sp);
             }
             // Generate variance and covariance:
-            rnd = cd_loc * rnd;
-            Ns.row(t+1) += rnd.t();
+            proc_rnd = cd_loc * proc_rnd;
+            Ns.row(t+1) += proc_rnd.t();
             Ns.row(t+1) += obs_rnd.t();
         }
     }
-    
+
     #ifdef _OPENMP
     }
     #endif
 
-    
+
     return N;
 }
 
 
 
 //' Melt a cube into a single data frame.
-//' 
+//'
 //' @param C Three-dimensional array that you want to melt into a two-dimensional
 //'     data frame.
 //'
 //' @return
-//' 
+//'
 //' @noRd
-//' 
+//'
 //[[Rcpp::export]]
-DataFrame melt_cube(arma::cube C) {
-    
-    uint C_rows = C.n_rows;
+DataFrame melt_cube(const arma::cube& C) {
+
+    uint32 C_rows = C.n_rows;
     arma::mat M(C.n_rows * C.n_slices, C.n_cols + 1);
-    for (uint i = 0; i < C.n_slices; i++) {
+    for (uint32 i = 0; i < C.n_slices; i++) {
         M(arma::span(i * C_rows, (i+1) * C_rows - 1), arma::span(0)).fill(i+1);
         M(arma::span(i * C_rows, (i+1) * C_rows - 1),
           arma::span(1, C.n_cols)) = C.slice(i);
     }
-    
+
     return M;
 }
 
 
 //' Generate data for simulations.
-//' 
+//'
 //' Generate multi-location, multi-species time series data.
-//' 
-//' 
+//'
+//'
 //' @param n_time Number of time steps.
 //' @param n_loc Number of locations.
 //' @param n_spp Number of species.
@@ -257,15 +264,15 @@ DataFrame melt_cube(arma::cube C) {
 //' @param sigma_obs Standard deviation for observation error. Defaults to 0.
 //' @param corr_method Method for determining correlations between species.
 //'     Options are "none", "phylo", or "random". Defaults to "none".
-//' 
-//' 
+//'
+//'
 //' @export
-//' 
-//' 
+//'
+//'
 //[[Rcpp::export]]
-List generate_data(const uint& n_time,
-                   const uint& n_loc,
-                   const uint& n_spp,
+List generate_data(const uint32& n_time,
+                   const uint32& n_loc,
+                   const uint32& n_spp,
                    const double& mean_b0,
                    const double& mean_b1,
                    const double& mean_rho,
@@ -296,10 +303,10 @@ List generate_data(const uint& n_time,
     // Observation error
     arma::vec obs_sigma_(n_spp);
     obs_sigma_.fill(sigma_obs);
-    
+
     // Initial population size (set to b0)
     arma::mat N0_mat_(b0_mat_);
-    
+
     arma::mat vcv_(n_spp, n_spp, arma::fill::eye);
     arma::cube vcv_cube_(n_spp, n_spp, n_loc, arma::fill::zeros);
     if (corr_method == "phylo") {
@@ -311,10 +318,10 @@ List generate_data(const uint& n_time,
         vcv_ = as<arma::mat>(vcv_SEXP_);
         vcv_ /= vcv_.diag()(0);
     } else if (corr_method == "random") {
-        uint n_corrs = ((n_spp - 1) / 2) * (1 + (n_spp - 1));
+        uint32 n_corrs = ((n_spp - 1) / 2) * (1 + (n_spp - 1));
         arma::vec rnd_phy = as<arma::vec>(Rcpp::runif(n_corrs, -1, 1));
-        for (uint i = 0, rnd_i = 0; i < n_spp; i++) {
-            for (uint j = i+1; j < n_spp; j++, rnd_i++) {
+        for (uint32 i = 0, rnd_i = 0; i < n_spp; i++) {
+            for (uint32 j = i+1; j < n_spp; j++, rnd_i++) {
                 vcv_(i,j) = rnd_phy(rnd_i);
                 vcv_(j,i) = rnd_phy(rnd_i);
             }
@@ -327,7 +334,7 @@ List generate_data(const uint& n_time,
     // Going from correlations to covariances:
     vcv_ *= (sigma_eps * sigma_eps);
     vcv_cube_.each_slice() += vcv_;
-    
+
     List out = List::create(_["X"] = X_,
                             _["N0_mat"] = N0_mat_,
                             _["b0_mat"] = b0_mat_,
@@ -335,6 +342,6 @@ List generate_data(const uint& n_time,
                             _["rho_mat"] = rho_mat_,
                             _["vcv_cube"] = vcv_cube_,
                             _["obs_sigma"] = obs_sigma_);
-    
+
     return out;
 }
--- src/sim_pops.cpp
+++ src/sim_pops.cpp
@@ -1,5 +1,5 @@
 #include <RcppArmadillo.h>
-#include <sitmo.h>  // sitmo::prng_engine
+#include <pcg/pcg_random.hpp> // pcg prng
 #include <random>  // normal distribution
 #include <vector>  // vector class
 #ifdef _OPENMP
@@ -7,45 +7,78 @@
 #endif
 
 #include "repts_types.h"
+#include "pcg.h"  // pcg seeding
 
 using namespace Rcpp;
 
 
 
-// Draw from normal distribution and fill row
-inline void rnd_row(arma::rowvec& rnd_vec,
-                    std::normal_distribution<double>& distr,
-                    sitmo::prng_engine& eng) {
-    for (uint i = 0; i < rnd_vec.n_elem; i++) {
-        rnd_vec(i) = distr(eng);
-    }
-}
 
 //' Simulate populations with competition.
+//'
+//' This does most of the work for the R-exported function below.
+//'
+//' @param N Output matrix of population abundances.
+//' @param distr Normal distribution generator from C++ STL.
+//' @param eng pcg32 object that generates random numbers.
+//' @inheritParams sim_pops
+//'
+//'
+//' @noRd
+//'
+void sim_pops_(arma::mat& N,
+               const arma::rowvec& r,
+               const arma::mat& alpha,
+               std::normal_distribution<double>& distr,
+               pcg32& eng) {
+
+    uint32 n_spp = N.n_cols;
+    uint32 max_t = N.n_rows - 1;
+
+    arma::rowvec rnd(n_spp, arma::fill::zeros);
+
+    for (uint32 t = 0; t < max_t; t++) {
+
+        for (uint32 i = 0; i < rnd.n_elem; i++) rnd(i) = distr(eng);
+
+        N.row(t+1) = N.row(t) % arma::exp(r - N.row(t) * alpha + rnd);
+    }
+
+    return;
+
+}
+
+
+
+//' Simulate populations with competition.
+//'
+//' @param max_t Number of time points to simulate.
+//' @param N0 Vector of starting population abundances, one for each species.
+//' @param r Vector of growth rates, one for each species.
+//' @param alpha Matrix of intra- and inter-specific density dependences.
+//' @param sigma Standard deviation of process error.
 //'
 //'
 //' @export
 //'
 //[[Rcpp::export]]
-arma::mat sim_pops(const uint& n_gen, const arma::rowvec& N0, const arma::rowvec& r,
-                   const arma::mat& alpha, const double& sigma, const uint& seed) {
-    uint n_spp = N0.n_elem;
+arma::mat sim_pops(const uint32& max_t, const arma::rowvec& N0, const arma::rowvec& r,
+                   const arma::mat& alpha, const double& sigma) {
+
+    uint32 n_spp = N0.n_elem;
     if (r.n_elem != n_spp || alpha.n_rows != n_spp || alpha.n_cols != n_spp) {
         stop("N0, r, and alpha lengths must be the same.");
     }
-    arma::mat N(n_gen + 1, n_spp);
+    pcg32 eng = seeded_pcg();
+
+    arma::mat N(max_t + 1, n_spp);
     N.row(0) = N0;
-    
-    std::normal_distribution<double> rnorm_distr(0.0, sigma);
-    sitmo::prng_engine engine(seed);
-    arma::rowvec rnd(n_spp, arma::fill::zeros);
-    
-    for (uint t = 0; t < n_gen; t++) {
-        rnd_row(rnd, rnorm_distr, engine);
-        N.row(t+1) = N.row(t) % arma::exp(r - N.row(t) * alpha + rnd);
-    }
-    
+
+    std::normal_distribution<double> distr(0.0, sigma);
+
+    sim_pops_(N, r, alpha, distr, eng);
+
     return N;
-    
+
 }
 
