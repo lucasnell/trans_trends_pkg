01d11e6add58362fe274bf91c4dd4cc9f888de26
Unknown <jsphillips2@wisc.edu>
2018-10-30 16:54:27 -0500

    Updated to fit lmm with AR1

--- under_constr/analyze_myv_arth.R
+++ under_constr/analyze_myv_arth.R
@@ -22,10 +22,12 @@ myv_arth2 = myv_arth %>%
     tbl_df() %>%
     mutate(log_count = log1p(count),
            log_midges = log1p(midges),
-           z_midges = (log_midges - mean(log_midges, na.rm=T))/sd(log_midges, na.rm=T)) %>%
+           z_midges = (log_midges - mean(log_midges, na.rm=T))/sd(log_midges, na.rm=T),
+           group = paste0(trans, dist, taxon) %>% as.factor() %>% as.integer()) %>%
     # group by taxon and them z-score count within taxon
     group_by(taxon) %>%
-    mutate(z_count = (log_count - mean(log_count, na.rm=T))/sd(log_count, na.rm=T))
+    mutate(z_count = (log_count - mean(log_count, na.rm=T))/sd(log_count, na.rm=T)) %>%
+    ungroup()
 
 # plot
 myv_arth2 %>%
@@ -37,10 +39,13 @@ myv_arth2 %>%
 
 # package data
 data_list = myv_arth2 %>%
-{list(n_obs = length(.$z_count),
+    arrange(trans, dist, taxon, year) %>%
+{list(n_obs = nrow(.),
       n_sp = .$taxon %>% unique %>% length(),
       map = .$taxon %>% as.factor() %>% as.integer(),
       taxon = .$taxon %>% as.factor(),
+      n_groups = length(.$group %>% unique()),
+      obs_per = {myv_arth2 %>% group_by(group) %>% summarize(n = length(z_count))}$n,
       x = .$z_midges,
       y = .$z_count)}
 
@@ -61,7 +66,7 @@ chains = 4
 iter = 2000
 
 # fit model
-fit = stan(file = "exec/lmm.stan", data = data_list, seed=1, chains = chains, iter = iter)
+fit = stan(file = "under_constr/lmm_AR.stan", data = data_list, seed=1, chains = chains, iter = iter)
 
 # summary of fit
 fit_summary = summary(fit, probs=c(0.16, 0.5, 0.84))$summary %>%
@@ -90,11 +95,22 @@ coefs %>%
     geom_hline(yintercept = 0, size = 0.3, alpha = 0.5)+
     geom_point(size = 2)+
     geom_errorbar(aes(ymin = `16%`, ymax = `84%`), width = 0)+
-    scale_y_continuous(limits=c(-0.5, 0.5))+
+    scale_y_continuous(limits=c(-0.25, 0.25))+
+    theme_bw()
+
+fit_summary %>%
+    filter(var %in% paste0("phi[",1:7,"]")) %>%
+    mutate(spec = 1:7) %>%
+    select(var, spec, `16%`,`50%`,`84%`) %>%
+    left_join(data_frame(spec = data_list$map %>% unique, taxon = data_list$taxon %>% unique)) %>%
+    ggplot(aes(taxon, `50%`))+
+    geom_hline(yintercept = 0, size = 0.3, alpha = 0.5)+
+    geom_point(size = 2)+
+    geom_errorbar(aes(ymin = `16%`, ymax = `84%`), width = 0)+
     theme_bw()
 
 test = coefs %>%
-    expand(taxon, z_midges = -2:1) %>%
+    expand(taxon, z_midges = -3:1.5) %>%
     left_join(coefs %>% select(taxon, `50%`)) %>%
     mutate(z_count = `50%`*z_midges)
 
@@ -102,7 +118,8 @@ myv_arth2 %>%
     ggplot(aes(z_midges, z_count))+
     facet_wrap(~taxon)+
     geom_point(alpha = 0.4, size = 1)+
-    geom_line(data = test, color = "firebrick", size = 0.8)+
+    geom_line(data = test, color = "red", size = 0.8)+
+    geom_smooth(method="lm", formula = y ~ x, se=F, color="blue", linetype = 2)+
     theme_bw()
 
 
