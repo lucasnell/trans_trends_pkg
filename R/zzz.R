f1111e579b4dfea3b34a539940cd6c701ba2eef6
Lucas Nell <lucas@lucasnell.com>
2019-02-05 13:03:43 -0600

    Re-built package

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onLoad <- function(libname, pkgname) {
     modules <- paste0("stan_fit4", names(stanmodels), "_mod")
-    if(length(stanmodels) > 0) ## for testing purposes
+    if (length(stanmodels) > 0) {## for testing purposes
         for (m in modules) {
             loadModule(m, what = TRUE)
         }
+    }
 }
