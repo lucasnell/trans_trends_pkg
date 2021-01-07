8c298e00fca22776df7983dd8123a7e3b8cad4f3
Lucas Nell <lucas@lucasnell.com>
2021-01-07 10:30:12 -0600

    Added posterior_mode function

--- R/armm.R
+++ R/armm.R
@@ -1,4 +1,20 @@
-
+#' Estimates the marginal parameter modes using kernel density estimation
+#'
+#' @param x numeric vector of posteriors
+#' @param adjust numeric, passed to density to adjust the bandwidth of the
+#'     kernal density
+#' @param ... other parameters to pass to `density`
+#'
+#' @export
+#'
+posterior_mode <- function (x, adjust = 0.1, ...) {
+    if (is.numeric(x) == FALSE & !is.null(dim(x))) {
+        warning("posterior_mode expects a numeric vector")
+    }
+    dx <- density(x, adjust = adjust, ...)
+    .mode <- dx$x[which.max(dx$y)]
+    return(.mode)
+}
 
 
 #' Version of lapply that returns the list flattened to a vector or array.
